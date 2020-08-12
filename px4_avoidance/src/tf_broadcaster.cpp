5e8e98f39965718c21368562ba473b4e810667ae
MohomedShalik <mohomedshalik2@gmail.com>
2020-08-12 19:01:59 +0530

    refactor:transform from base_link frame to map frame for the 2D costmap

--- px4_avoidance/src/tf_broadcaster.cpp
+++ px4_avoidance/src/tf_broadcaster.cpp
@@ -4,19 +4,19 @@
 
 
 
-nav_msgs::Odometry odom_frame;
+nav_msgs::Odometry mapFrame;
 
 void odom_callb(const nav_msgs::Odometry::ConstPtr odom)
 {
 
-    odom_frame.pose.pose.position.x = odom->pose.pose.position.x;
-    odom_frame.pose.pose.position.y = odom->pose.pose.position.y;
-    odom_frame.pose.pose.position.z = odom->pose.pose.position.z;
+  mapFrame.pose.pose.position.x = odom->pose.pose.position.x;
+  mapFrame.pose.pose.position.y = odom->pose.pose.position.y;
+  mapFrame.pose.pose.position.z = odom->pose.pose.position.z;
 
-    odom_frame.pose.pose.orientation.w = odom->pose.pose.orientation.w;
-    odom_frame.pose.pose.orientation.x = odom->pose.pose.orientation.x;
-    odom_frame.pose.pose.orientation.y = odom->pose.pose.orientation.y;
-    odom_frame.pose.pose.orientation.z = odom->pose.pose.orientation.z;
+  mapFrame.pose.pose.orientation.w = odom->pose.pose.orientation.w;
+  mapFrame.pose.pose.orientation.x = odom->pose.pose.orientation.x;
+  mapFrame.pose.pose.orientation.y = odom->pose.pose.orientation.y;
+  mapFrame.pose.pose.orientation.z = odom->pose.pose.orientation.z;
 
 }
 
@@ -24,27 +24,27 @@ void odom_callb(const nav_msgs::Odometry::ConstPtr odom)
 int main(int argc , char** argv)
 {
 
-    ros::init(argc , argv , "iris_tf_broadcaster");
+	ros::init(argc , argv , "iris_tf_broadcaster");
 
-    ros::NodeHandle node;
-    
+	ros::NodeHandle node;
+	
 
-    tf::TransformBroadcaster br;
-    tf::Transform transform;
+	tf::TransformBroadcaster br;
+	tf::Transform transform;
 
-    ros::Rate rate(1000.0);
+	ros::Rate rate(1000.0);
 
-    ros::Subscriber odom_sub = node.subscribe<nav_msgs::Odometry>("mavros/local_position/odom" , 30 , odom_callb);
+	ros::Subscriber odomSub = node.subscribe<nav_msgs::Odometry>("mavros/local_position/odom" , 30 , odom_callb);
 
-    while(node.ok()){
-        
-        transform.setOrigin(tf::Vector3(odom_frame.pose.pose.position.x , odom_frame.pose.pose.position.y , odom_frame.pose.pose.position.z));
-        transform.setRotation( tf::Quaternion(odom_frame.pose.pose.orientation.x , odom_frame.pose.pose.orientation.y, odom_frame.pose.pose.orientation.z , odom_frame.pose.pose.orientation.w ) );
-        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", "base_link"));
+	while(node.ok()){
+			
+		transform.setOrigin(tf::Vector3(mapFrame.pose.pose.position.x , mapFrame.pose.pose.position.y , mapFrame.pose.pose.position.z));
+		transform.setRotation( tf::Quaternion(mapFrame.pose.pose.orientation.x , mapFrame.pose.pose.orientation.y, mapFrame.pose.pose.orientation.z , mapFrame.pose.pose.orientation.w ) );
+		br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", "base_link"));
 
-        rate.sleep();
-        ros::spinOnce();
-    }
-    return 0;
+		rate.sleep();
+		ros::spinOnce();
+	}
+	return 0;
 
 }
